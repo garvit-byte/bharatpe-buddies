0c608104d24778a80a9de0eedde0d16aaaa31ace
Chandresh Gelda <chandresh.gelda@bharatpe.com>
2021-03-05 18:34:37 +0530

    lat long end point

--- src/main/java/com/bharatpebuddies/bharatpebuddies/controller/LatLongController.java
+++ src/main/java/com/bharatpebuddies/bharatpebuddies/controller/LatLongController.java
@@ -5,6 +5,7 @@ import com.bharatpebuddies.bharatpebuddies.dto.ResponseDTO;
 import com.bharatpebuddies.bharatpebuddies.service.LatLongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
@@ -13,6 +14,7 @@ public class LatLongController {
     @Autowired
     LatLongService latLongService;
 
+    @GetMapping("/get_list_of_merchant")
     public ResponseDTO getListOfMerchantNearMe(@RequestParam("lat") String lat , @RequestParam("longi") String longi){
      return latLongService.getListOfNearMerchants(lat,longi);
     }
