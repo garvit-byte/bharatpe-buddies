5c12c2baea4788738299b5b58f7fe9661929bc79
Chandresh Gelda <chandresh.gelda@bharatpe.com>
2021-03-06 15:22:50 +0530

    ignore

--- src/main/java/com/bharatpebuddies/bharatpebuddies/dto/MerchantRequestDto.java
+++ src/main/java/com/bharatpebuddies/bharatpebuddies/dto/MerchantRequestDto.java
@@ -1,10 +1,14 @@
 package com.bharatpebuddies.bharatpebuddies.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MerchantRequestDto {
     private Long merchantId;
     private String message;
