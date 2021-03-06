220e4f720c7dfb32ef12ad174b9c95729e3f147f
Chandresh Gelda <chandresh.gelda@bharatpe.com>
2021-03-06 15:20:41 +0530

    method change

--- src/main/java/com/bharatpebuddies/bharatpebuddies/BharatpeBuddiesApplication.java
+++ src/main/java/com/bharatpebuddies/bharatpebuddies/BharatpeBuddiesApplication.java
@@ -1,8 +1,10 @@
 package com.bharatpebuddies.bharatpebuddies;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class BharatpeBuddiesApplication {
 
--- src/main/java/com/bharatpebuddies/bharatpebuddies/dto/MerchantRequestDto.java
+++ src/main/java/com/bharatpebuddies/bharatpebuddies/dto/MerchantRequestDto.java
@@ -1,14 +1,14 @@
 package com.bharatpebuddies.bharatpebuddies.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class MerchantRequestDto {
-
     private Long merchantId;
     private String message;
     private String businessCategory;
     private String businessName;
     private String image;
-
 }
